7b56c777fa9ffc888af01af26d5bd7a57065c585
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-07-04 21:26:26 +0200

    read html file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var fs = require('fs'), express = require('express');
 
 var app = express.createServer(express.logger());
+var utf8 = "utf8"
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('index.html', utf8, function(err, data){
+    if(err) 
+      throw err;
+    buffer = new Buffer(data);
+  	response.send(buffer.toString(utf8));
+  });
 });
 
 var port = process.env.PORT || 5000;
